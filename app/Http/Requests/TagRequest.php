36a9e29a2f4614bf2f1a6aaada9c2fbe222485f3
Luigi <luigicruz.dev@gmail.com>
2021-10-15 14:00:31 +0800

    Create TagRequest

---
+++ app/Http/Requests/TagRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return match($this->method()) {
+            'POST' => $this->store(),
+            'PUT', 'PATCH' => $this->update()
+        };
+    }
+
+    /**
+     * Get the validation rules that apply to the post request.
+     *
+     * @return array
+     */
+    public function store()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'min:2',
+                'unique:tags,name'
+            ],
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the put/patch request.
+     *
+     * @return array
+     */
+    public function update()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'min:2',
+                Rule::when($this->tag->exists, 'sometimes')
+            ],
+        ];
+    }
+}
